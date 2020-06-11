dcb35d11d8759a4ea9db0b046f4229e2a3686d8c
Dennis Grabowski <dennisgrabowski@gmx.de>
2020-06-11 18:56:40 +0200

    lintfix: components: validation props in destructuring by providing type information

--- application/src/components/page-layout/navbar.tsx
+++ application/src/components/page-layout/navbar.tsx
@@ -4,25 +4,31 @@ import styles from 'styles/navbar.module.css'
 import {RouteObject, RouteNames, routesMap} from 'configs/routes'
 
 // helper functions to reduce duplicate code of applied css styles
-type NavLinkProps = {
-  to: string
-  exact?: boolean
+type Props = {
   children?: React.ReactNode
 }
 
-const PageName: FunctionComponent = props => (
-  <li className={styles.pageName}>{props.children}</li>
+// This is called an intersection type, which combines multiple types
+// into one. Extending a type with another type is not allowed in Typescript
+// and the workaround with introducing an interface seems rather "hacky"
+type NavLinkProps = Props & {
+  to: string
+  exact?: boolean
+}
+
+const PageName: FunctionComponent<Props> = ({children}: Props) => (
+  <li className={styles.pageName}>{children}</li>
 )
 
-const NavElement: FunctionComponent = props => (
-  <li className={styles.subPages}>{props.children}</li>
+const NavElement: FunctionComponent<Props> = ({children}: Props) => (
+  <li className={styles.subPages}>{children}</li>
 )
 
 const NavLinkStyled: FunctionComponent<NavLinkProps> = ({
   to,
   exact,
   children,
-}) => (
+}: NavLinkProps) => (
   <NavLink {...exact} to={to} className={styles.navLink}>
     {children}
   </NavLink>
